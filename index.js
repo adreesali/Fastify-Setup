6aae6916f91559529f689d22a3842108c7feb582
adrees-ali <109959486+adreesali@users.noreply.github.com>
2023-11-27 21:45:18 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("hello wrold");
+console.log("How are you brother");
+console.log("what are you doing");
+const count = 5;
+console.log('count: hi', count);
---
+++ index.ts
@@ -0,0 +1,8 @@
+console.log("hello wrold");
+
+console.log("How are you brother");
+console.log("what are you doing");
+ 
+   const count = 5; 
+
+console.log('count: hi', count);
